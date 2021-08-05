591c4d736f3da99a6cd824c31eb05d85552ffa95
Hayden Robinson <HRobinson@stu.kerikerihigh.ac.nz>
2021-08-06 09:54:37 +1200

    nibba

--- game.py
+++ game.py
@@ -3,14 +3,17 @@ import arcade
 SPRITE_SCALING_PLATFORM = 0.25
 SPRITE_SCALING_PLAYER = 0.25
 SPRITE_SCALING_ENEMY = 0.15
-PLAYER_JUMP_SPEED = 9
+PLAYER_JUMP_SPEED = 11
 GRAVITY = 0.5
 SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 640
-PLAYER_MOVEMENT_SPEED = 5
+PLAYER_MOVEMENT_SPEED = 3.5
 RIGHT_FACING = 0
 LEFT_FACING = 1
 ENEMY_SPEED = 2.5
+PLAYER_FRAMES = 4
+PLAYER_FRAMES_PER_TEXTURE = 4
+TILE_SCALING = 1.75
 
 def load_texture_pair(filename):
     
@@ -26,17 +29,37 @@ class Character(arcade.Sprite):
 
         self.character_face_direction = RIGHT_FACING
         self.cur_texture = 0
+        self.virtual_texture = 0
         self.idle_texture_pair = load_texture_pair("bach.png")
         self.texture = self.idle_texture_pair[self.character_face_direction]
         self.scale = SPRITE_SCALING_PLAYER
-        return
+
+        self.walk_textures = []
+        for i in range(4):
+            texture = load_texture_pair(f"./Assets/Animations/sprite_{i}.png")
+            self.walk_textures.append(texture)
+        
+        self.texture = self.idle_texture_pair[0]
     
     def update_animation(self, delta_time = 1/60):
         if self.change_x < 0 and self.character_face_direction == RIGHT_FACING:
             self.character_face_direction = LEFT_FACING
         elif self.change_x > 0 and self.character_face_direction == LEFT_FACING:
             self.character_face_direction = RIGHT_FACING
-        self.texture = self.idle_texture_pair[self.character_face_direction] 
+        
+        if self.change_x == 0:
+            self.texture = self.idle_texture_pair[self.character_face_direction] 
+        else:
+            self.virtual_texture += 1
+            if self.virtual_texture > PLAYER_FRAMES * PLAYER_FRAMES_PER_TEXTURE:
+                self.cur_texture = 0
+                self.virtual_texture = 0
+            if (self.virtual_texture +1) % PLAYER_FRAMES_PER_TEXTURE == 0:
+                self.cur_texture = self.virtual_texture // PLAYER_FRAMES_PER_TEXTURE
+                self.texture = self.walk_textures[self.cur_texture][
+                    self.character_face_direction
+                ]
+
 
 class Enemy(arcade.Sprite):
     def __init__(self, x, y):
@@ -48,7 +71,7 @@ class Enemy(arcade.Sprite):
         self.start_x = x
         self.change_y = ENEMY_SPEED
         self.start_y = y
-        self.patrol = 50
+        self.patrol = 150
 
     def update(self):
         self.center_x += self.change_x
@@ -60,9 +83,6 @@ class Enemy(arcade.Sprite):
         
     # def on_draw(self):
     #     pass
-
-
-
 
 class MyGame(arcade.Window):
 
@@ -85,17 +105,26 @@ class MyGame(arcade.Window):
         self.wall_list = arcade.SpriteList()
         self.enemy_list = arcade.SpriteList()
 
-        wall = arcade.Sprite("New Piskel (2).png", SPRITE_SCALING_PLATFORM)
-        wall.center_x = 300
-        wall.center_y = 200
-        self.wall_list.append(wall)
-
         self.player_sprite = Character()
-        self.player_sprite.center_x = SCREEN_WIDTH/2
-        self.player_sprite.center_y = SCREEN_HEIGHT/2
+        self.player_sprite.center_x = 550
+        self.player_sprite.center_y = 698
         self.player_list.append(self.player_sprite)
-    
-        enemy = Enemy(125, 300)
+
+        
+
+        map_name = ":resources:tmx_maps/map.tmx"
+
+        platforms_layer_name = 'Ground'
+
+        my_map = arcade.tilemap.read_tmx("Assets\Maps\Gangsta map.tmx")
+
+
+        self.wall_list = arcade.tilemap.process_layer(map_object=my_map,
+                                                     layer_name=platforms_layer_name,
+                                                     scaling=TILE_SCALING,
+                                                     use_spatial_hash=True)
+
+        enemy = Enemy(2040, 1032)
         self.enemy_list.append(enemy)
 
         self.background = arcade.load_texture("Background.png")
@@ -110,7 +139,7 @@ class MyGame(arcade.Window):
     def on_draw(self):
         arcade.start_render()
 
-        arcade.draw_lrwh_rectangle_textured(0, 0,
+        arcade.draw_lrwh_rectangle_textured(self.get_viewport()[0], self.get_viewport()[2],
                                             SCREEN_WIDTH, SCREEN_HEIGHT,
                                             self.background)
     
@@ -118,23 +147,16 @@ class MyGame(arcade.Window):
         self.wall_list.draw()
         self.enemy_list.draw()
 
+
+
     def update(self, delta_time):
 
+        self.set_viewport(self.player_sprite.center_x - SCREEN_WIDTH/2, self.player_sprite.center_x + SCREEN_WIDTH/2, self.player_sprite.center_y - SCREEN_HEIGHT/2, self.player_sprite.center_y + SCREEN_HEIGHT/2)
         self.physics_engine.update()
         self.player_sprite.update()
         self.enemy_list.update()
 
-        if self.player_sprite.center_x < SPRITE_SCALING_PLAYER:
-            self.player_sprite.center_x = SPRITE_SCALING_PLAYER
 
-        if self.player_sprite.center_x > SCREEN_WIDTH - SPRITE_SCALING_PLAYER:
-            self.player_sprite.center_x = SCREEN_WIDTH - SPRITE_SCALING_PLAYER
-
-        if self.player_sprite.center_y < SPRITE_SCALING_PLAYER:
-            self.player_sprite.center_y = SPRITE_SCALING_PLAYER
-
-        if self.player_sprite.center_y > SCREEN_HEIGHT - SPRITE_SCALING_PLAYER:
-            self.player_sprite.center_y = SCREEN_HEIGHT - SPRITE_SCALING_PLAYER
 
         self.player_sprite.update_animation(delta_time)
 
@@ -148,6 +170,15 @@ class MyGame(arcade.Window):
                 else:
                     self.setup()
 
+        for enemy in self.enemy_list:
+            
+            if len(arcade.check_for_collision_with_list(enemy, self.wall_list)) > 0:
+                enemy.change_x *= -1
+
+    def on_mouse_press(self, x, y, button, modifiers):
+        map_mouse_x = self.get_viewport()[0] + x
+        map_mouse_y = self.get_viewport()[2] + y
+        print(f'{map_mouse_x = } {map_mouse_y = } ')
 
     def on_key_press(self, symbol, modifiers):
         if symbol == arcade.key.LEFT:
@@ -155,8 +186,8 @@ class MyGame(arcade.Window):
         elif symbol == arcade.key.RIGHT:
             self.player_sprite.change_x = PLAYER_MOVEMENT_SPEED
         elif symbol == arcade.key.UP:
-            if self.physics_engine.can_jump():
-                self.player_sprite.change_y = PLAYER_JUMP_SPEED
+            # if self.physics_engine.can_jump():
+            self.player_sprite.change_y = PLAYER_JUMP_SPEED
 
     def on_key_release(self, symbol, modifiers):
         
@@ -167,10 +198,9 @@ class MyGame(arcade.Window):
         elif symbol == arcade.key.DOWN:
             self.player_sprite.change_y = 0
 
-
 def main():
-    Window = MyGame()
-    Window.setup()
+    window = MyGame()
+    window.setup()
     arcade.run()
 
 if __name__ == "__main__":
